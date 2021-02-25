96eadbdf1a8fdb5a767b5d9801a313dc2535d84d
cebil <cebil333@gmail.com>
2021-02-25 12:55:52 +0530

    finiddfjf

---
+++ src/views/Lading.js
@@ -0,0 +1,373 @@
+import React from "react";
+
+import Navbar from "../components/Navbar.js";
+import Footer from "../components/Footer.js";
+
+export default function Landing() {
+  
+  
+  return (
+    <>
+      <Navbar transparent />
+      <main>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
+            style={{
+              minHeight: "95vh"
+            }}>
+          <div className="absolute top-0 w-full h-full bg-top bg-cover"
+              style={{
+                backgroundImage: "url('https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80')"
+              }}>
+            <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+          </div>
+          <div className="container relative mx-auto">
+              <div className="items-center flex flex-wrap">
+                <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                  <div >
+                    <h1 className="text-white font-semibold text-5xl">
+                      Feel the <span className='text-orange-500'>POWER</span>
+                    </h1>
+                    <p className="mt-4 text-lg text-gray-300">
+                      Welcone to the Flex gym.We are a firness and training center that focuses on pushing you to your absolute limit.Download our complete brochure and start today! 
+                    </p>
+                    <a href="/" className="bg-transparent hover:bg-orange-500 text-orange-500 font-semibold hover:text-white py-3 px-4 border border-white-500 hover:border-transparent rounded inline-block mt-5" >Download Brochure</a>
+                  </div>
+                </div>
+                
+
+              </div>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+            style={{ height: "70px", transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+        </div>
+
+        
+
+        <section id="about" className="relative py-20 bg-black text-white">
+          
+
+          <div className="container mx-auto px-4">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full md:w-4/12 ml-auto mr-auto px-4" data-aos="fade-right">
+                <img
+                  alt="..."
+                  className="max-w-full rounded-lg shadow-lg"
+                  src="https://images.unsplash.com/photo-1550345332-09e3ac987658?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                />
+              </div>
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4" data-aos="fade-left">
+                <div className="md:pr-12">
+                  
+                  <small className="text-orange-500">About our gym</small>
+                  <h3 className="text-4xl uppercase font-bold">
+                    Safe Body building
+                  </h3>
+                  <p className="mt-4 text-lg leading-relaxed ">
+                    The extension comes with three pre-built pages to help you
+                    get started faster. You can change the text and images and
+                    you're good to go.
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="font-semibold inline-block py-3 mr-3 rounded-full text-yellow-500">
+                            <i className="fas fa-dumbbell fa-2x"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-xl">
+                            The latest & quality equipments
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="font-semibold inline-block py-3 mr-3 rounded-full text-yellow-500">
+                            <i className="fas fa-home fa-2x"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-xl">
+                            dressroom service
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="font-semibold inline-block py-3 mr-3 rounded-full text-yellow-500">
+                            <i className="fas fa-users fa-2x"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-xl">
+                            3 professional trainers
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        <section className="pt-20 pb-48">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap justify-center text-center mb-24">
+              <div className="w-full lg:w-6/12 px-4">
+                <h2 className="text-4xl font-semibold upperclass">
+                  Meet our trainers
+                </h2>
+                <p className="text-lg leading-relaxed m-4 ">
+                  Our trainers are here to dedicate the time and effort that you need to get in the best shape of your life.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-4/12 lg:mb-0 mb-12 px-4" data-aos="flip-right">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    src={require("./assets/gym.jpeg")}
+                    className="shadow-lg rounded-full max-w-full mx-auto"
+                    style={{ maxheigth: "300px" }}
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">
+                      Harshad
+                    </h5>
+                    <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                      Gym trainer
+                    </p>
+                    <div className="mt-6">
+                      <button
+                        className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </button>
+                      <button
+                        className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-facebook-f"></i>
+                      </button>
+                      <button
+                        className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-dribbble"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-4/12 lg:mb-0 mb-12 px-4" data-aos="flip-right">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    src="./assets/gym.jpeg"
+                    className="shadow-lg rounded-full max-w-full mx-auto"
+                    style={{ maxheigth: "240px" }}
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">
+                      Romina Hadid
+                    </h5>
+                    <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                      Gym trainer
+                    </p>
+                    <div className="mt-6">
+                      <button
+                        className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-google"></i>
+                      </button>
+                      <button
+                        className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-facebook-f"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-4/12 lg:mb-0 mb-12 px-4" data-aos="flip-right">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    src={require("./assets/gym.jpeg")}
+                    className="shadow-lg rounded-full max-w-full mx-auto"
+                    style={{ maxheigth: "240px" }}
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">
+                      Alexa Smith
+                    </h5>
+                    <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                      UI/UX Designer
+                    </p>
+                    <div className="mt-6">
+                      <button
+                        className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-google"></i>
+                      </button>
+                      <button
+                        className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </button>
+                      <button
+                        className="bg-gray-800 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                      >
+                        <i className="fab fa-instagram"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </section>
+
+        <section className="pb-20 relative block bg-black text-white">
+          <div
+            className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+            style={{ height: "80px", transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div className="container mx-auto px-4 lg:pt-24 lg:pb-64 pb-20 pt-20">
+            <div className="flex flex-wrap text-center justify-center">
+              <div className="w-full lg:w-6/12 px-4">
+                <h2 className="text-4xl font-semibold text-white uppercase">
+                  Contact us
+                </h2>
+                <p className="text-lg leading-relaxed mt-4 mb-4 ">
+                  Contact us for taking membership for a good starting 
+                </p>
+              </div>
+            </div>
+            
+              
+          </div>
+        </section>
+        <section className="relative block py-24 lg:pt-0 bg-black">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300" data-aos="fade-up-right">
+                  <div className="flex-auto p-5 lg:p-10 bg-orange-500 text-white">
+                    <h4 className="text-2xl font-semibold">
+                      Want to work with us?
+                    </h4>
+                    <p className="leading-relaxed mt-1 mb-4 ">
+                      Complete this form and we will get back to you in 24 hours.
+                    </p>
+                    <div className="relative w-full mb-3 mt-8">
+                      <label
+                        className="block uppercase text-xs font-bold mb-2"
+                        htmlFor="full-name"
+                      >
+                        Full Name
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        placeholder="Full Name"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-xs font-bold mb-2"
+                        htmlFor="email"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        className="px-3 py-3 placeholder-gray-400 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        placeholder="Email"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-xs font-bold mb-2"
+                        htmlFor="message"
+                      >
+                        Message
+                      </label>
+                      <textarea
+                        rows="4"
+                        cols="80"
+                        className="px-3 py-3 placeholder-gray-400 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        placeholder="Type a message..."
+                      />
+                    </div>
+                    <div className="text-center mt-6">
+                      <button
+                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                        type="button"
+                        style={{ transition: "all .15s ease" }}
+                      >
+                        Send Message
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
